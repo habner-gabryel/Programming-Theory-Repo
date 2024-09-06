860e292ecd88659822d2d9d6c9c87aac1a6513cf
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-09-05 21:44:20 -0300

    create first character class

---
+++ Programming-Theory-Repo/Assets/Scripts/Objects/Champions/Knight.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Champion
+{
+    public Weapon knightBaseWeapon;
+    public Knight() {
+        this.characterName = "Knight";
+        this.maxLife = 5;
+        this.forceMultiplier = 1.4F;
+        this.forceMovementSpeed = 3F;
+    }
+
+    void Start() {
+
+        standardWeapon = knightBaseWeapon;
+    }
+}
